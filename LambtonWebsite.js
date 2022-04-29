755c54218d7c87368f58cc3d42b62638d85d5fc3
karandave710 <karandave710@gmail.com>
2022-04-29 18:14:28 +0530

    to test it on my own

--- LambtonWebsite.js
+++ LambtonWebsite.js
@@ -1,10 +1,12 @@
+///<reference types="cypress" />  
 describe("LambtonSuite",function()
 {
   it("LaunchLambton",function()
   
   {
       cy.visit("https://www.lambtoncollege.ca")
-      cy.title().should('eq',"Welcome | Lambton College")
+      cy.title().should('eq',"Welcome | Lambton College") // line # 1 is to get auto suggestions from cypress
+      
   })
 
 })
